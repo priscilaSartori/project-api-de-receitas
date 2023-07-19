ea7427d3e1c8cc0193615e325b2317a8e36b6e18
Priscila <priscilasartori2@gmail.com>
2023-07-19 01:33:31 -0300

    feat: Desenvolva o endpoint POST /user

--- src/recipes-api/Controllers/UserController.cs
+++ src/recipes-api/Controllers/UserController.cs
@@ -38,7 +38,8 @@ public class UserController : ControllerBase
   [HttpPost]
   public IActionResult Create([FromBody] User user)
   {
-    throw new NotImplementedException();
+    this._service.AddUser(user);
+    return Created("user", user);
   }
 
   // "8 - Sua aplicação deve ter o endpoint PUT /user
