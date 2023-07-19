c604bd31d0bf71ab295580d2c3cad92ddde9d801
Priscila <priscilasartori2@gmail.com>
2023-07-19 00:00:36 -0300

    feat: Desenvolva o endpoint GET /recipe

--- src/recipes-api/Controllers/RecipesController.cs
+++ src/recipes-api/Controllers/RecipesController.cs
@@ -12,48 +12,48 @@ namespace recipes_api.Controllers;
 [ApiController]
 [Route("recipe")]
 public class RecipesController : ControllerBase
-{    
-    public readonly IRecipeService _service;
-    
-    public RecipesController(IRecipeService service)
-    {
-        this._service = service;        
-    }
+{
+  public readonly IRecipeService _service;
 
-    // 1 - Sua aplicação deve ter o endpoint GET /recipe
-    //Read
-    [HttpGet]
-    public IActionResult Get()
-    {
-        throw new NotImplementedException();    
-    }
+  public RecipesController(IRecipeService service)
+  {
+    this._service = service;
+  }
 
-    // 2 - Sua aplicação deve ter o endpoint GET /recipe/:name
-    //Read
-    [HttpGet("{name}", Name = "GetRecipe")]
-    public IActionResult Get(string name)
-    {                
-        throw new NotImplementedException();
-    }
+  // 1 - Sua aplicação deve ter o endpoint GET /recipe
+  //Read
+  [HttpGet]
+  public IActionResult Get()
+  {
+    return Ok(this._service.GetRecipes());
+  }
 
-    // 3 - Sua aplicação deve ter o endpoint POST /recipe
-    [HttpPost]
-    public IActionResult Create([FromBody]Recipe recipe)
-    {
-        throw new NotImplementedException();
-    }
+  // 2 - Sua aplicação deve ter o endpoint GET /recipe/:name
+  //Read
+  [HttpGet("{name}", Name = "GetRecipe")]
+  public IActionResult Get(string name)
+  {
+    throw new NotImplementedException();
+  }
 
-    // 4 - Sua aplicação deve ter o endpoint PUT /recipe
-    [HttpPut("{name}")]
-    public IActionResult Update(string name, [FromBody]Recipe recipe)
-    {
-        throw new NotImplementedException();
-    }
+  // 3 - Sua aplicação deve ter o endpoint POST /recipe
+  [HttpPost]
+  public IActionResult Create([FromBody] Recipe recipe)
+  {
+    throw new NotImplementedException();
+  }
 
-    // 5 - Sua aplicação deve ter o endpoint DEL /recipe
-    [HttpDelete("{name}")]
-    public IActionResult Delete(string name)
-    {
-        throw new NotImplementedException();
-    }    
+  // 4 - Sua aplicação deve ter o endpoint PUT /recipe
+  [HttpPut("{name}")]
+  public IActionResult Update(string name, [FromBody] Recipe recipe)
+  {
+    throw new NotImplementedException();
+  }
+
+  // 5 - Sua aplicação deve ter o endpoint DEL /recipe
+  [HttpDelete("{name}")]
+  public IActionResult Delete(string name)
+  {
+    throw new NotImplementedException();
+  }
 }
